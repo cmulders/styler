b62c9f8bfdf79c076ffbe241ca0a1fd977234fd2
cmulders <coen.mulders@gmail.com>
2021-07-23 11:02:26 +0200

    Implement tokenizer

--- src/styler.py
+++ src/styler.py
@@ -1,8 +1,11 @@
+from contextlib import contextmanager
 import codecs
+import enum
+import re
 from typing import (
     BinaryIO,
+    Callable,
     Iterable,
-    NamedTuple,
     TextIO,
     TypeVar,
     Union,
@@ -145,3 +148,629 @@ def decode(
     return io.TextIOWrapper(buffered, encoding=codec_info.name, errors="replace")
 
 
+# Ref: https://www.w3.org/TR/css-syntax-3/#tokenization
+@dataclass(frozen=True)
+class Token:
+    position: int
+
+
+class EOF(Token):
+    pass
+
+
+class Comma(Token):
+    pass
+
+
+class Colon(Token):
+    pass
+
+
+class Semicolon(Token):
+    pass
+
+
+class CDO(Token):
+    # <!--
+    pass
+
+
+class CDC(Token):
+    # -->
+    pass
+
+
+class OpenParenthesis(Token):
+    pass
+
+
+class CloseParenthesis(Token):
+    pass
+
+
+class OpenCurlyBracket(Token):
+    pass
+
+
+class CloseCurlyBracket(Token):
+    pass
+
+
+class OpenBracket(Token):
+    pass
+
+
+class CloseBracket(Token):
+    pass
+
+
+@dataclass(frozen=True)
+class ValueToken(Token):
+    value: str
+
+
+class Comment(ValueToken):
+    pass
+
+
+class Whitespace(ValueToken):
+    pass
+
+
+class Delim(ValueToken):
+    pass
+
+
+class Ident(ValueToken):
+    pass
+
+
+class String(ValueToken):
+    pass
+
+
+class BadString(Token):
+    pass
+
+
+class HashType(enum.Enum):
+    UNRESTRICTED = enum.auto()
+    ID = enum.auto()
+
+
+@dataclass(frozen=True)
+class Hash(ValueToken):
+    hash_type: HashType = HashType.UNRESTRICTED
+
+
+class Function(ValueToken):
+    pass
+
+
+class Url(ValueToken):
+    pass
+
+
+class AtKeyword(ValueToken):
+    pass
+
+
+class BadUrl(Token):
+    pass
+
+
+@dataclass(frozen=True)
+class Percentage(Token):
+    value: float
+
+
+class NumericType(enum.Enum):
+    INTEGER = enum.auto()
+    NUMBER = enum.auto()
+
+
+@dataclass(frozen=True)
+class Number(Token):
+    value: Union[int, float]
+    numeric_type: NumericType
+
+
+@dataclass(frozen=True)
+class Dimension(Number):
+    dimension: str
+
+
+# Ref: https://www.w3.org/TR/css-syntax-3/#whitespace
+WHITESPACE = string.whitespace
+
+# Ref: https://www.w3.org/TR/css-syntax-3/#non-printable-code-point
+NON_PRINTABLE_CODE_POINT = (
+    # U+0000 NULL and U+0008 BACKSPACE inclusive
+    "\x00\x01\x02\x03\x04\x05\x06\x07\x08"
+    # U+000B LINE TABULATION
+    "\x0b"
+    # U+000E SHIFT OUT and U+001F INFORMATION SEPARATOR ONE inclusive
+    "\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f"
+    # U+007F DELETE
+    "\x7f"
+)
+
+T = TypeVar("T")
+
+
+class Tokenizer:
+    def __init__(self, content: str) -> None:
+        self.content = content
+        self.pos = 0
+        self.exhausted = False
+        self.errors = []
+
+    @classmethod
+    def from_reader(cls, reader: TextIO):
+        return cls(reader.read())
+
+    @property
+    def remaining(self):
+        return len(self.content) - self.pos
+
+    @contextmanager
+    def memo(self):
+        pos = self.pos
+        try:
+            yield
+        finally:
+            self.pos = pos
+
+    def with_memo(self, fn: Callable[["Tokenizer"], T]) -> T:
+        with self.memo():
+            return fn(self)
+
+    def peek(self, n: int = 1) -> str:
+        # Returns the next N characters without modifying the position
+        return self.content[self.pos : self.pos + n]
+
+    def advance(self, n: int) -> int:
+        # Increase the position
+        self.pos += n
+        return self.pos
+
+    def consume(self, n: int) -> str:
+        # Returns the next N characters and increases the position
+        start = self.pos
+        end = self.advance(n)
+        return self.content[start:end]
+
+    def consume_all(self) -> str:
+        start = self.pos
+        self.pos = len(self.content)  # Set to end
+        return self.content[start:]
+
+    def content_iter(self) -> Iterable[str]:
+        return iter(self.content[self.pos :])
+
+    @property
+    def eof(self) -> bool:
+        return self.pos >= len(self.content)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self) -> Token:
+        if self.exhausted:
+            raise StopIteration
+
+        if self.eof:
+            self.exhausted = True
+            return EOF(self.pos)
+
+        token = self.next_token()
+        if token is not None:
+            return token
+
+        raise StopIteration
+
+    def next_token(self) -> Optional[Token]:
+        # Ref: https://www.w3.org/TR/css-syntax-3/#consume-token
+        c = self.peek(1)
+        pos = self.pos
+        if c == "/":
+            if self.peek(2) == "/*":
+                return self.consume_comment()
+        elif c in WHITESPACE:
+            return self.consume_whitespace()
+        elif c in '"':
+            return self.consume_string()
+        elif c in "#":
+            with self.memo():
+                self.advance(1)
+                is_ident = self.at_ident_start()
+                is_name = is_ident or self.is_name_char(self.peek(1))
+                is_eof = self.eof
+            if is_ident:
+                return self.consume_hash(HashType.ID)
+            elif not is_eof and is_name:
+                return self.consume_hash(HashType.UNRESTRICTED)
+        elif c in "'":
+            return self.consume_string()
+        elif c == "(":
+            self.advance(1)
+            return OpenParenthesis(pos)
+        elif c == ")":
+            self.advance(1)
+            return CloseParenthesis(pos)
+        elif c == "+":
+            if self.at_number_start():
+                return self.consume_numeric()
+        elif c == ",":
+            self.advance(1)
+            return Comma(pos)
+        elif c == "-":
+            if self.at_number_start():
+                return self.consume_numeric()
+            if self.peek(3) == "-->":
+                self.advance(3)
+                return CDC(pos)
+            if self.at_ident_start():
+                return self.consume_ident_like()
+        elif c == ".":
+            if self.at_number_start():
+                return self.consume_numeric()
+        elif c == ":":
+            self.advance(1)
+            return Colon(pos)
+        elif c == ";":
+            self.advance(1)
+            return Semicolon(pos)
+        elif c == "<":
+            if self.peek(4) == "<!--":
+                self.advance(4)
+                return CDO(pos)
+        elif c == "@":
+            with self.memo():
+                self.advance(1)
+                is_ident = self.at_ident_start()
+            if is_ident:
+                self.advance(1)
+                name = self.consume_name()
+                return AtKeyword(pos, name)
+        elif c == "[":
+            self.advance(1)
+            return OpenBracket(pos)
+        elif c == "]":
+            self.advance(1)
+            return CloseBracket(pos)
+        elif c == "{":
+            self.advance(1)
+            return OpenCurlyBracket(pos)
+        elif c == "}":
+            self.advance(1)
+            return CloseCurlyBracket(pos)
+        elif c in string.digits:
+            return self.consume_numeric()
+        elif self.is_name_start(c):
+            return self.consume_ident_like()
+
+        # If not anything above, default to delim token
+        return Delim(pos, self.consume(1))
+
+    def at_escape(self) -> bool:
+        peeked = self.peek(2)
+        if len(peeked) != 2:
+            return False
+        return self.is_escape(peeked)
+
+    @classmethod
+    def is_escape(cls, chars: str) -> bool:
+        assert len(chars) >= 2
+
+        c1, c2 = chars[:2]
+        if c1 != "\\":
+            return False
+        elif c2 == "\n":
+            return False
+
+        return True
+
+    @classmethod
+    def is_non_printable(cls, c: str) -> bool:
+        assert len(c) == 1
+        return c in NON_PRINTABLE_CODE_POINT
+
+    @classmethod
+    def is_whitespace(cls, c: str) -> bool:
+        assert len(c) == 1
+        return c in WHITESPACE
+
+    @classmethod
+    def is_name_char(cls, c: str) -> bool:
+        if c == "":
+            return False
+        assert len(c) == 1
+        return cls.is_name_start(c) or c in string.digits + "-"
+
+    @classmethod
+    def is_name_start(cls, c: str) -> bool:
+        assert len(c) == 1
+        return c in string.ascii_letters + "_" or c > "\u0080"
+
+    def at_number_start(self) -> bool:
+        # Ref: https://www.w3.org/TR/css-syntax-3/#check-if-three-code-points-would-start-a-number
+        if self.remaining < 2:
+            # We need at least 2 code points
+            return False
+
+        elif self.remaining == 2:
+            c1, c2 = self.peek(2)
+            c3 = None
+        else:
+            c1, c2, c3 = self.peek(3)
+
+        if c1 in "+-":
+            if c2 in string.digits:
+                return True
+            elif c2 == "." and c3 is not None:
+                return c3 in string.digits
+
+            return False
+        elif c1 == ".":
+            return c2 in string.digits
+
+        return c1 in string.digits
+
+    def at_ident_start(self) -> bool:
+        # Ref: https://www.w3.org/TR/css-syntax-3/#would-start-an-identifier
+        if self.remaining < 2:
+            return False
+        elif self.remaining == 2:
+            c1, c2 = self.peek(2)
+            c3 = None
+        else:
+            c1, c2, c3 = self.peek(3)
+
+        if c1 == "-":
+            c1, c2 = c2, c3  # Shift the code points left
+
+        if self.is_name_start(c1) or c1 == "-":
+            return True
+        elif c2 is not None and self.is_escape(c1 + c2):
+            return True
+        else:
+            return False
+
+    def consume_comment(self):
+        # Ref: https://www.w3.org/TR/css-syntax-3/#consume-comments
+        start = self.pos
+        self.advance(2)  # Consume the /*
+
+        value = ""
+        while not self.eof and self.peek(2) != "*/":
+            value += self.consume(1)
+
+        if self.peek(2) == "*/":
+            self.advance(2)
+
+        return Comment(start, value)
+
+    def consume_whitespace(self):
+        # Ref: https://www.w3.org/TR/css-syntax-3/#whitespace
+        start = self.pos
+
+        value = ""
+        while not self.eof and self.is_whitespace(self.peek(1)):
+            value += self.consume(1)
+
+        return Whitespace(start, value)
+
+    def consume_string(self):
+        # Ref: https://www.w3.org/TR/css-syntax-3/#consume-string-token
+        ending = self.consume(1)
+        start = self.pos
+
+        value = ""
+        while not self.eof and self.peek(1) != ending:
+            if self.at_escape():
+                value += self.consume_escape()
+            elif self.peek(1) == "\n":
+                return BadString(start)
+            else:
+                value += self.consume(1)
+
+        if self.eof or self.peek(1) != ending:
+            return BadString(start)
+        else:
+            self.advance(1)
+            return String(start, value)
+
+    def consume_escape(self) -> str:
+        assert self.consume(1) == "\\"
+
+        if self.eof:
+            return "\N{REPLACEMENT CHARACTER}"
+
+        if self.peek(1) not in string.hexdigits:
+            # Return code point, as it is not a hex digit
+            return self.consume(1)
+
+        hex_chars = ""
+        while not self.eof and self.peek(1) in string.hexdigits and len(hex_chars) < 6:
+            hex_chars += self.consume(1)
+
+        if not self.eof and self.is_whitespace(self.peek(1)):
+            # If the next token is whitespace, consume it
+            self.advance(1)
+
+        codepoint = chr(int(hex_chars, 16))
+        # Check valid unicode range and it is not a surrogate (https://infra.spec.whatwg.org/#surrogate)
+        if "\u0000" < codepoint <= "\U0010ffff" and not (
+            "\uD800" <= codepoint <= "\uDFFF"
+        ):
+            return codepoint
+        else:
+            # Invalid range, use fallback
+            return "\N{REPLACEMENT CHARACTER}"
+
+    def consume_name(self) -> str:
+        # Ref: https://www.w3.org/TR/css-syntax-3/#consume-name
+        name = ""
+        while not self.eof and self.is_name_char(self.peek(1)) or self.at_escape():
+            if self.at_escape():
+                name += self.consume_escape()
+            else:
+                name += self.consume(1)
+
+        assert name, "Did not parse a name."
+
+        return name
+
+    def consume_hash(self, hash_type: HashType):
+        start = self.pos
+        assert self.consume(1) == "#"
+
+        return Hash(start, self.consume_name(), hash_type)
+
+    def consume_number(self) -> Tuple[Union[int, float], NumericType]:
+        n_type = NumericType.INTEGER
+        _repr = ""
+        value = 0
+
+        def _consume_digits() -> Iterable[str]:
+            while not self.eof and self.peek(1) in string.digits:
+                yield self.consume(1)
+
+            return ""
+
+        if self.peek(1) in "+-":
+            _repr += self.consume(1)
+
+        _repr += "".join(_consume_digits())
+
+        p2 = self.peek(2)
+        if len(p2) == 2 and p2[0] == "." and p2[1] in string.digits:
+            n_type = NumericType.NUMBER
+            _repr += self.consume(2)
+            _repr += "".join(_consume_digits())
+
+        p3 = self.peek(3)
+        if len(p3) >= 2 and p3[0] in "eE":
+            signed = len(p3) == 3 and p3[1] in "+-" and p3[2] in string.digits
+            if signed or p3[1] in string.digits:
+                n_type = NumericType.NUMBER
+                _repr += self.consume(2)
+                _repr += "".join(_consume_digits())
+
+        # Ref: https://www.w3.org/TR/css-syntax-3/#convert-a-string-to-a-number
+        components = re.fullmatch(
+            r"""
+            (?P<sign>[+-]?)
+            (?P<integer>\d*)
+            (?P<decimal>\.?)
+            (?P<fractional>\d*)
+            (?P<expind>[eE]?)
+            (?P<expsign>[+-]?)
+            (?P<exponent>\d*)
+            """,
+            _repr,
+            re.VERBOSE,
+        )
+        if components is None:
+            raise ValueError("Not a number")
+        s = -1 if components.group("sign") == "-" else 1
+        i = int(components.group("integer") or "0")
+        f = int(components.group("fractional") or "0")
+        d = len(components.group("fractional"))
+        t = -1 if components.group("expsign") == "-" else 1
+        e = int(components.group("exponent") or "0")
+        value = s * (i + f * pow(10, -d)) * pow(10, t * e)
+
+        return value, n_type
+
+    def consume_numeric(self):
+        # Ref: https://www.w3.org/TR/css-syntax-3/#consume-a-numeric-token
+        start = self.pos
+        value, n_type = self.consume_number()
+
+        if self.at_ident_start():
+            unit = self.consume_name()
+            return Dimension(start, value, n_type, unit)
+
+        if self.peek(1) == "%":
+            self.advance(1)
+            return Percentage(start, value)
+
+        return Number(start, value, n_type)
+
+    def consume_ident_like(self):
+        # Ref: https://www.w3.org/TR/css-syntax-3/#consume-an-ident-like-token
+        pos = self.pos
+        name = self.consume_name()
+
+        if name.lower() == "url" and self.peek(1) == "(":
+            self.advance(1)
+            while all(self.is_whitespace(c) for c in self.peek(2)):
+                self.advance(1)
+            chrs = self.peek(2)
+            if any(c in "\"'" for c in chrs):
+                return Function(pos, name)
+            else:
+                return self.consume_url(pos)
+
+        elif self.peek(1) == "(":
+            self.advance(1)
+            return Function(pos, name)
+        else:
+            return Ident(pos, name)
+
+    def consume_bad_url_remnants(self):
+        # Ref: https://www.w3.org/TR/css-syntax-3/#consume-remnants-of-bad-url
+        while not self.eof and self.peek(1) != ")":
+            if self.at_escape():
+                self.consume_escape()
+            else:
+                self.consume(1)
+
+        if self.peek(1) == ")":
+            self.consume(1)
+
+        return
+
+    def consume_url(self, pos: int = None):
+        # Ref: https://www.w3.org/TR/css-syntax-3/#consume-url-token
+        assert pos is not None, "Must pass the start of the url(."
+        value = ""
+        while not self.eof:
+            if self.peek(1) == "\\":
+                if self.at_escape():
+                    value += self.consume_escape()
+                else:
+                    self.errors.append("Invalid escape while parsing Url")
+                    self.consume_bad_url_remnants()
+                    return BadUrl(pos)
+
+            c = self.consume(1)
+
+            if c == ")":
+                return Url(pos, value)
+            elif self.is_whitespace(c):
+                # Whitespace is not allowed whithin a url, so parse the rest untill ')'
+                while not self.eof and self.is_whitespace(self.peek(1)):
+                    self.advance(1)
+
+                if self.eof:
+                    self.errors.append("Unexpected EOF while parsing Url")
+                    return Url(pos, value)
+                elif self.peek(1) == ")":
+                    self.advance(1)
+                    return Url(pos, value)
+
+                self.consume_bad_url_remnants()
+                return BadUrl(pos)
+
+            elif c in "\"'(" or self.is_non_printable(c):
+                self.consume_bad_url_remnants()
+                return BadUrl(pos)
+            else:
+                # Append valid character to url
+                value += c
+
+        self.errors.append("Unexpected EOF while parsing Url")
+        return BadUrl(pos)
--- tests/test_styler.py
+++ tests/test_styler.py
@@ -1,3 +1,4 @@
+from typing import Iterable
 import unittest
 
 import io
@@ -11,3 +12,220 @@ class DecoderTestCase(unittest.TestCase):
         )
         decoded = styler.decode(stream)
         self.assertEqual('@charset "ISO-8859-5"; @é', decoded.read())
+
+
+class TokenizerTestCase(unittest.TestCase):
+    def assertTokenEqual(self, css, token):
+        expected_tokens = [token] if not isinstance(token, Iterable) else list(token)
+
+        tokenizer = styler.Tokenizer(css)
+        found_tokens = list(tokenizer)
+
+        if not expected_tokens or not isinstance(expected_tokens[-1], styler.EOF):
+            # Strip EOF if we did not specify
+            found_tokens = found_tokens[:-1]
+
+        self.assertListEqual(
+            found_tokens,
+            expected_tokens,
+            msg=f"Tokenization of `{css}` did not result in expected tokens.",
+        )
+
+    def test_empty(self):
+        self.assertTokenEqual("", [])
+        self.assertTokenEqual("", [styler.EOF(0)])
+
+    def test_comment(self):
+        self.assertTokenEqual("/* test */", styler.Comment(0, " test "))
+        self.assertTokenEqual("/**/", styler.Comment(0, ""))
+        self.assertTokenEqual("/* test /* nest */", styler.Comment(0, " test /* nest "))
+        self.assertTokenEqual("/*test \n /*test", styler.Comment(0, "test \n /*test"))
+
+    def test_whitespace(self):
+        self.assertTokenEqual(" ", styler.Whitespace(0, " "))
+        self.assertTokenEqual(
+            "/*   \t*/ /*",
+            [
+                styler.Comment(0, "   \t"),
+                styler.Whitespace(8, " "),
+                styler.Comment(9, ""),
+                styler.EOF(11),
+            ],
+        )
+
+    def test_string(self):
+        self.assertTokenEqual('"Test"', styler.String(1, "Test"))
+        self.assertTokenEqual("'Test'", styler.String(1, "Test"))
+        self.assertTokenEqual("'T\"est'", styler.String(1, 'T"est'))
+        self.assertTokenEqual('"T\'est"', styler.String(1, "T'est"))
+
+    def test_badstring(self):
+        self.assertTokenEqual('"T\n', [styler.BadString(1), styler.Whitespace(2, "\n")])
+        # Unterminated
+        self.assertTokenEqual('"T', styler.BadString(1))
+
+    def test_curly_brackets(self):
+        self.assertTokenEqual(
+            "/**/{ }",
+            [
+                styler.Comment(0, ""),
+                styler.OpenCurlyBracket(4),
+                styler.Whitespace(5, " "),
+                styler.CloseCurlyBracket(6),
+            ],
+        )
+
+    def test_parenthesis(self):
+        self.assertTokenEqual(
+            "( ) )",
+            [
+                styler.OpenParenthesis(0),
+                styler.Whitespace(1, " "),
+                styler.CloseParenthesis(2),
+                styler.Whitespace(3, " "),
+                styler.CloseParenthesis(4),
+            ],
+        )
+
+    def test_plus_not_a_number(self):
+        self.assertTokenEqual(
+            "+A",
+            [styler.Delim(0, "+"), styler.Ident(1, "A")],
+        )
+
+    def test_number_percentage(self):
+        self.assertTokenEqual("100%", styler.Percentage(0, 100))
+        self.assertTokenEqual("-100%", styler.Percentage(0, -100))
+        self.assertTokenEqual("15.5%", styler.Percentage(0, 15.5))
+        self.assertTokenEqual("-15.5%", styler.Percentage(0, -15.5))
+        self.assertTokenEqual("2e3%", styler.Percentage(0, 2e3))
+        self.assertTokenEqual("-2e3%", styler.Percentage(0, -2e3))
+
+    def test_number_dimension(self):
+        NUMBER = styler.NumericType.NUMBER
+        INTEGER = styler.NumericType.INTEGER
+        # self.assertTokenEqual("100em", styler.Dimension(0, 100, INTEGER, "em"))
+        # self.assertTokenEqual("-100px", styler.Dimension(0, -100, INTEGER, "px"))
+        # self.assertTokenEqual("15.5text", styler.Dimension(0, 15.5, NUMBER, "text"))
+        # self.assertTokenEqual("-15.5pt", styler.Dimension(0, -15.5, NUMBER, "pt"))
+        self.assertTokenEqual("2e3em", styler.Dimension(0, 2e3, NUMBER, "em"))
+        self.assertTokenEqual("-2e3rem", styler.Dimension(0, -2e3, NUMBER, "rem"))
+
+    def test_number_integer(self):
+        INTEGER = styler.NumericType.INTEGER
+        self.assertTokenEqual("+1", styler.Number(0, 1, INTEGER))
+        self.assertTokenEqual("-1", styler.Number(0, -1, INTEGER))
+
+    def test_number_number(self):
+        NUMBER = styler.NumericType.NUMBER
+        self.assertTokenEqual("+1.5", styler.Number(0, 1.5, NUMBER))
+        self.assertTokenEqual("-1.5", styler.Number(0, -1.5, NUMBER))
+        self.assertTokenEqual(".5", styler.Number(0, 0.5, NUMBER))
+        self.assertTokenEqual("-.5", styler.Number(0, -0.5, NUMBER))
+
+    def test_number_number_exp(self):
+        NUMBER = styler.NumericType.NUMBER
+        # self.assertTokenEqual("2e2", styler.Number(0, 2e2, NUMBER))
+        # self.assertTokenEqual("-2e2", styler.Number(0, -2e2, NUMBER))
+        # self.assertTokenEqual("2e-2", styler.Number(0, 2e-2, NUMBER))
+        # self.assertTokenEqual("-2e-2", styler.Number(0, -2e-2, NUMBER))
+        self.assertTokenEqual("2.5e2", styler.Number(0, 2.5e2, NUMBER))
+        self.assertTokenEqual("-2.5e2", styler.Number(0, -2.5e2, NUMBER))
+        self.assertTokenEqual("2.5e-2", styler.Number(0, 2.5e-2, NUMBER))
+        self.assertTokenEqual("-2.5e-2", styler.Number(0, -2.5e-2, NUMBER))
+
+    def test_hash_without_name(self):
+        self.assertTokenEqual("#", styler.Delim(0, "#"))
+
+    def test_hash_id(self):
+        self.assertTokenEqual("#test", styler.Hash(0, "test", styler.HashType.ID))
+        self.assertTokenEqual("#--test", styler.Hash(0, "--test", styler.HashType.ID))
+
+    def test_hash_unrestricted(self):
+        self.assertTokenEqual("#0red", styler.Hash(0, "0red"))
+        self.assertTokenEqual("#-0red", styler.Hash(0, "-0red"))
+
+    def test_comma(self):
+        self.assertTokenEqual(",", styler.Comma(0))
+        self.assertTokenEqual(
+            "#a,#",
+            [
+                styler.Hash(0, "a", styler.HashType.ID),
+                styler.Comma(2),
+                styler.Delim(3, "#"),
+            ],
+        )
+
+    def test_cdo(self):
+        self.assertTokenEqual("<!--", styler.CDO(0))
+
+    def test_cdc(self):
+        self.assertTokenEqual("-->", styler.CDC(0))
+
+    def test_ident_url(self):
+        self.assertTokenEqual(
+            "url('')",
+            [
+                styler.Function(0, "url"),
+                styler.String(5, ""),
+                styler.CloseParenthesis(6),
+            ],
+        )
+
+    def test_minus_ident(self):
+        self.assertTokenEqual("-a", styler.Ident(0, "-a"))
+        self.assertTokenEqual("--a", styler.Ident(0, "--a"))
+        self.assertTokenEqual("--a-b", styler.Ident(0, "--a-b"))
+
+    def test_escapes(self):
+        self.assertTokenEqual("-\\000394a", styler.Ident(0, "-\u0394a"))
+
+    def test_function(self):
+        self.assertTokenEqual(
+            "func1('arg1', 'arg2')",
+            [
+                styler.Function(0, "func1"),
+                styler.String(7, "arg1"),
+                styler.Comma(12),
+                styler.Whitespace(13, " "),
+                styler.String(15, "arg2"),
+                styler.CloseParenthesis(20),
+            ],
+        )
+
+    def test_not_a_url(self):
+        self.assertTokenEqual(
+            "url('arg1')",
+            [
+                styler.Function(0, "url"),
+                styler.String(5, "arg1"),
+                styler.CloseParenthesis(10),
+            ],
+        )
+
+    def test_url(self):
+        self.assertTokenEqual("url()", styler.Url(0, ""))
+        # Url should ignore whitespace
+        self.assertTokenEqual("url( \t\n)", styler.Url(0, ""))
+
+    def test_colon(self):
+        self.assertTokenEqual(":", styler.Colon(0))
+
+    def test_semicolon(self):
+        self.assertTokenEqual(";", styler.Semicolon(0))
+
+    def test_declaration(self):
+        self.assertTokenEqual(
+            "border-width: 10px;",
+            [
+                styler.Ident(0, "border-width"),
+                styler.Colon(12),
+                styler.Whitespace(13, " "),
+                styler.Dimension(14, 10, styler.NumericType.INTEGER, "px"),
+                styler.Semicolon(18),
+            ],
+        )
+
+    def test_at_keyword(self):
+        self.assertTokenEqual("@media", styler.AtKeyword(0, "media"))
+        self.assertTokenEqual("@--some", styler.AtKeyword(0, "--some"))
